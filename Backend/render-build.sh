cbd34f9e61083d1b5a80543edd6437c112c2053f
Tsili-spec <76454356+Tsili-spec@users.noreply.github.com>
2025-07-18 05:55:06 +0100

    Update render-build.sh

--- Backend/render-build.sh
+++ Backend/render-build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Install system-level dependency needed by PyAudio
-apt-get update && apt-get install -y portaudio19-dev
-
-# Now install Python dependencies
+pyenv install 3.10.13
+pyenv global 3.10.13
+python --version
 pip install -r requirements.txt
 
