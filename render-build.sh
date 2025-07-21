c4ad2a504b21c5d1cace9bcb26f358ba4586406a
Asongna-frank <87443470+Asongna-frank@users.noreply.github.com>
2025-07-21 09:42:49 +0100

    reorganizing files

--- render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install system-level dependency needed by PyAudio
-apt-get update && apt-get install -y portaudio19-dev
-
-# Now install Python dependencies
-pip install -r requirements.txt
-
